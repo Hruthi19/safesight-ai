40666920953f6edce6076329f5297f90879a24d1
Hruthi19 <hruthimuggalla@gmail.com>
2025-10-02 16:53:16 -0400

    fixed eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,7 @@
 // eslint.config.js
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import jest from "eslint-plugin-jest";
+import reactPlugin from "eslint-plugin-react";
+import jestPlugin from "eslint-plugin-jest";
 import globals from "globals";
 
 export default [
@@ -14,12 +14,16 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
-      requireConfigFile: false,
       globals: {
-        window: "readonly",
-        document: "readonly",
-        test: "readonly",
-        expect: "readonly",
+        // window: "readonly",
+        // document: "readonly",
+        // test: "readonly",
+        // expect: "readonly",
+        // browser: true,
+        // node: true
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest
       },
       parserOptions: {
         ecmaFeatures: {
@@ -28,13 +32,14 @@ export default [
       },
     },
     plugins: {
-      react,
-      jest,
+      react: reactPlugin,
+      jest: jestPlugin,
     },
     rules: {
       "react/react-in-jsx-scope": "off",
       "no-unused-vars": "warn",
       "jest/expect-expect": "warn",
+      "no-undef": "error",
     },
     settings: {
       react: {
